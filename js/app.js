29babe70152c5a2f49376bbb9ca7ca7ea5faacfd
Monika <monika.stobieniecka1@wp.pl>
2016-08-17 17:00:46 +0200

    Adding new page-dashboard. Creating a header.

--- js/app.js
+++ js/app.js
@@ -2,43 +2,44 @@ $(document).foundation();
 $(document).ready(function() {
 
     function showWarningMessage () {
-        $('#val-message-container').fadeIn("slow");
-        $('#val-message-container').removeClass('hidden-message');
-        $('#val-message-container').delay(5000).fadeOut();
+        $('#val-message-container').fadeIn("slow").removeClass('hidden-message').delay(5000).fadeOut();
     };
 
-    function sentAjax(passw) {
-        if (passw.length > 0) {
-            var errorMessage;
-            $('#val-message-container').addClass('hidden-message');
-            $.ajax({
-                type: "post",
-                data: {
-                    login: "efi",
-                    password: passw
-                },
-                url: "https://efigence-camp.herokuapp.com/api/login",
-                error: function(response) {
-                    var obj = JSON.parse(response.responseText);
-                    $('.warning-message').text(obj.message);
-                    showWarningMessage('#val-message-container');
-                },
-                success: function(response) {
-                    window.location.replace("http://google.com");
+    function sendAjax(passw) {
 
-                }
-            });
-        } else {
+        if (passw.length < 1){
             showWarningMessage();
             $('.warning-message').text('Podaj hasło!');
+            return;
         }
+
+        var errorMessage;
+
+        $('#val-message-container').addClass('hidden-message');
+
+        $.ajax({
+            type: "post",
+            data: {
+                login: "efi",
+                password: passw
+            },
+            url: "https://efigence-camp.herokuapp.com/api/login",
+            error: function(response) {
+                var obj = JSON.parse(response.responseText);
+                $('.warning-message').text(obj.message);
+                showWarningMessage('#val-message-container');
+            },
+            success: function(response) {
+                window.location.replace("http://google.com");
+
+            }
+        });
     };
 
-    $('.go-btn').on('click', function(event) {
+    $('.go-btn').on('submit', function(event) {
         event.preventDefault();
         var passw = $('#password').val();
         sentAjax(passw);
-
     });
 
     $('.customer-image').on('click', function(event) {
