249eb09ed5998b0875096c55336f5445d9532e2e
Monika <monika.stobieniecka1@wp.pl>
2017-04-23 01:20:30 +0200

    Added gameplay page and styles for it

--- js/app.js
+++ js/app.js
@@ -197,4 +197,10 @@ $(document).ready(function() {
         $('.search-icon-input').hide();        
         $('.search-icon-link').show();
     });
+    $('.incompleted-btn').on('click', function(event) {
+        $('.gameplay-panel').find('.completed').fadeOut('slow');
+    })
+    $('.all-btn').on('click', function (event) {
+        $('.gameplay-panel').find('.card-item').fadeIn('slow');
+    })
 });
